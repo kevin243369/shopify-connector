8c2faa506accef03c7334333ec8dac77dd9bcf55
Kevin Lee <kevin@impersuasion.com>
2020-12-19 11:17:13 +0800

    simpler result test

--- app.py
+++ app.py
@@ -48,7 +48,7 @@ def orders():
     
     shopify = shopifydata(data)
     shopify_orders = shopify.get_all_orders()
-    return jsonify({"result": shopify_orders.to_dict(orient='records')})
+    return '{ "name":"John", "age":30, "city":"New York"}'
 
     # response = app.response_class(
     #     response=json.dumps(shopify_orders.to_dict(orient='records')),
@@ -126,7 +126,7 @@ def products():
         return jsonify(error=str(e)), 404
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8080, host= '0.0.0.0')
+    app.run(debug=True, port=5000, host= 'localhost')
 
     # gcloud run deploy shopify-connector --image $DOCKER_IMG --platform managed --region $REGION --allow-unauthenticated
     # http://localhost:5000/orders?api_key=962b7f897d3284cb320d4d194d27dccf&secret=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&password=a94cdaad05ee6bb41320f99735c04336&shop=harryandchewie&start_date=2018-01-01&end_date=2020-12-01
